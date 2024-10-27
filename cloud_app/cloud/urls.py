e35cd6cacb0d5ef4d1de3cb14f7daebb60283580
BenjaminCIR <90195757+BenjaminCIR@users.noreply.github.com>
2024-10-27 15:51:44 +0100

    views

--- cloud_app/cloud/urls.py
+++ cloud_app/cloud/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # la route polls/ utiliser le fichier urls.py du dossier polls (d'où le polls.urls)
+    path("polls/", include("polls.urls")), 
+    path("admin/", admin.site.urls),
 ]
